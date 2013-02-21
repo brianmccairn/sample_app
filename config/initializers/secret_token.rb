9981da96aa66674723d184bc121499f1661f8242
briantherubyist <brian.mccairn@medicapp.com>
2013-02-21 21:26:09 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '047e1aed9a753ce1c261d7a11504c4b227748c2c0b412299701851825e4ed4dd10029e4054ed4e43524955e36478800f8961ec4b76ce49d5f94264d5d9e1fbaf'
